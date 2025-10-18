159202865f3d3942b565965a9018803e8cf3a5dd
ARYAN KUMAR SAHU <aryanaks0007@gmail.com>
2025-10-19 00:26:12 +0530

    Time: 4 ms (84.19%) | Memory: 50.9 MB (37.70%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -1,28 +1,27 @@
 class Solution {
-    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count=0;
-        if(ruleKey.equals("type")){
-            for(int i=0;i<items.size();i++){
+    public int countMatches(List<List<String>> items, String rulekey, String ruleValue) {
+        int count = 0;
+        if(rulekey.equals("type") ){
+            for(int i =0 ; i<items.size();i++){
                 if(items.get(i).get(0).equals(ruleValue)){
                     count++;
                 }
             }
         }
-        if(ruleKey.equals("color")){
-            for(int i=0;i<items.size();i++){
+        if(rulekey.equals("color") ){
+            for(int i =0 ; i<items.size();i++){
                 if(items.get(i).get(1).equals(ruleValue)){
                     count++;
                 }
             }
         }
-        if(ruleKey.equals("name")){
-            for(int i=0;i<items.size();i++){
+        if(rulekey.equals("name") ){
+            for(int i =0 ; i<items.size();i++){
                 if(items.get(i).get(2).equals(ruleValue)){
                     count++;
                 }
             }
         }
-
         return count;
     }
 }
