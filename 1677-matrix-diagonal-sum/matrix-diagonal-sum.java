3044d8b966e2aa7de6f34381c585f83de67f760c
ARYAN KUMAR SAHU <aryanaks0007@gmail.com>
2025-10-21 23:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (62.50%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -1,37 +1,19 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
+        int sum = 0;
         int n = mat.length;
-        int sum = 0 ;
         if(n == 1){
             return mat[0][0];
         }
-        if(n%2 == 0){
-            for(int i =0 ; i < n ;i++){
-                for(int j = 0 ; j < n ; j++){
-                    if(i == j){
-                        sum+=mat[i][j];
-                    }
-                }
-                for(int j = n -1 ; j>0 ; j++){
-                    sum+= mat[i][n-1-i];
-                    break;
-                }
-            }
+        for(int i =0 ; i < n ; i++){
+            sum += mat[i][i];
+            sum += mat[i][n-1-i]; 
         }
-        if(n%2!=0){
-            for(int i =0 ; i < n ;i++){
-                for(int j = 0 ; j < n ; j++){
-                    if(i == j){
-                        sum+=mat[i][j];
-                    }
-                }
-                for(int j = n -1 ; j>0 ; j++){
-                    sum+= mat[i][n-1-i];
-                    break;
-                }
-            }
-            sum -= mat[n/2][n/2];
+        if(n%2 !=0){
+            sum-= mat[n/2][n/2];
         }
-        return sum;
+
+        
+    return sum;
     }
 }
