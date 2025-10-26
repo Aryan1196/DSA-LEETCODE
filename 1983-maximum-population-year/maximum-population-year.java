79d629480f556bc0dc05b70d4292be8aa8c6c9c7
ARYAN KUMAR SAHU <aryanaks0007@gmail.com>
2025-10-26 20:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (80.71%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,28 +1,25 @@
-/*in this question we have to find how many people were alive in all the 100 years and return the year in which maximum people were alive*/
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int[] years = new int[101];
-        for(int i =0 ; i<logs.length ; i++){
-            int birth = logs[i][0];
-            int death = logs[i][1];
-
-            if(birth > 1950){
-                years[birth - 1950] += 1;
+        int[] arr = new int[101];
+        for (int i =0 ; i < logs.length  ; i++){
+            if(logs[i][0] > 1950){
+                arr[logs[i][0] - 1950] += 1;
             }
-            if(death < 2050){
-                years[death - 1950]+=-1;
+            if(logs[i][1] < 2050){
+                arr[logs[i][1] - 1950] += -1;
             }
         }
-        int max = 0 ; int current = 0 ; int res = 0 ; 
-        for(int i = 0 ; i < years.length ; i++){
-            current += years[i];
-            years[i] = current ;
+        int max = 0;
+        int j = 0;
+        int current = 0;
+        for(int i = 0 ; i < arr.length ;i++){
+            current += arr[i];
+            arr[i] = current;
             if(current > max){
                 max = current;
-                res = i ;
+                j = i;
             }
         }
-        
-        return res+1950  ;
+        return j + 1950;
     }
 }
