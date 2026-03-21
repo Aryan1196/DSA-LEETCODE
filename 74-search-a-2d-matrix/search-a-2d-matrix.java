20811efea448b4577cf2142d606c616c4fb2042b
ARYAN KUMAR SAHU <aryanaks0007@gmail.com>
2026-03-21 18:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (36.52%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,35 +1,35 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n  = matrix[0].length;
-        int top = 0; int bottom = m-1;
-        int row = 0;
-        while(top <= bottom){
-            row = (bottom+top)/2;
-            if(target > matrix[row][n-1]){
-                top = row+1;
-            }
-            else if(target < matrix[row][0]){
-                bottom = row - 1;
-            }
-            else{
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int start = 0;
+        int end = n - 1;
+        int row = -1;
+        while(start <= end ){
+            int mid = start + (end - start)/2;
+            if(target >= matrix[mid][0] && target <= matrix[mid][m-1]){
+                row = mid;
                 break;
             }
+            else if(target < matrix[mid][0]){
+                end = mid - 1;
+            }
+            else if(target > matrix[mid][m-1]){
+                start = mid + 1;
+            }
         }
-        if(top > bottom){
-            return false;
-        }
-        int left = 0; int right = n-1;
+        if(row == -1) return false;
+        int left = 0 , right = m-1;
         while(left <= right){
-            int mid = (left+right)/2;
-            if(target > matrix[row][mid]){
-                left = mid+1 ;
-            }
-            else if (target < matrix[row][mid]){
-                right = mid - 1;
-            }
-            else{
+            int mid = left + (right - left)/2;
+            if(matrix[row][mid] == target){
                 return true;
+            }
+            else if(matrix[row][mid] < target){
+                left = mid + 1;
+            }
+            else if(matrix[row][mid] > target){
+                right = mid - 1;
             }
         }
         return false;
